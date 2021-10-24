2feccc65bd343b9ba024ecdbb4aab5d9219dd3b4
LucasSouza00 <lucascosta1239@hotmail.com>
2021-10-23 21:42:27 -0300

    OrderStatus enum

---
+++ src/main/java/com/example/course/entities/enums/OrderStatus.java
@@ -0,0 +1,31 @@
+package com.example.course.entities.enums;
+
+public enum OrderStatus {
+	
+	// enumerações com os atributos específicados com números
+	WAITING_PAYMENT(1),
+	PAID(2),
+	SHIPPED(3),
+	DELIVERED(4),
+	CANCELED(5);
+	
+	private int code;
+	
+	private OrderStatus(int code) {
+		this.code = code;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	
+	// Método para converter um valor numérico para tipo enumerado
+	public static OrderStatus valueOf(int code) {
+		for (OrderStatus value : OrderStatus.values()) {
+			if (value.getCode() == code) {
+				return value;
+			}
+		}
+		throw new IllegalArgumentException("Invalid OrderStatus code");
+	}
+}
